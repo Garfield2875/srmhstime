01aed7b210cc58458fc2d8cd15e951ce45878bb3
Garfield2875 <TheNerdNextDoor2875@gmail.com>
2023-02-17 09:20:45 -0500

    fixed period checker

--- functions.js
+++ functions.js
@@ -1,22 +1,22 @@
-const oneStart = [7,25];
-const oneEnd = [8,48];
-const homeStart = [8,54];
-const homeEnd = [9,24];
-const twoStart = [9,30];
-const twoEnd = [10,53];
-const lunchStartA = [10,59];
-const lunchEndA = [11,29];
-const threeStartA = [11,32];
-const threeEndA = [12,56];
-const lunchStartB = [11,43];
-const lunchEndB = [12,10];
-const threeStartB1 = [10,59];
-const threeEndB1 = [11,39];
-const threeStartB2 = [12,13];
-const threeEndB2 = [12,56];
-const fourStart = [1,2];
-const fourEnd = [2,25];
-let period = "Error no Period";
+const oneStart = 26700;
+const oneEnd = 31680;
+const homeStart = 32040;
+const homeEnd = 33840;
+const twoStart = 34200;
+const twoEnd = 39180;
+const lunchStartA = 39540;
+const lunchEndA = 41340;
+const threeStartA = 41520;
+const threeEndA = 46560;
+const lunchStartB = 42180;
+const lunchEndB = 43800;
+const threeStartB1 = 39540;
+const threeEndB1 = 41940;
+const threeStartB2 = 43980;
+const threeEndB2 = 46560;
+const fourStart = 46920;
+const fourEnd = 51900;
+var period = "Error no Period";
 
 function startTime() {
     const today = new Date();
@@ -37,16 +37,46 @@ function checkTime(i) {
     return i;
     }
 
+/*
+function checkPeriod() { 
+
+
+
+
+}
+*/
+
 function checkPeriod() { 
     const today = new Date();
     let h = today.getHours();
     let m = today.getMinutes();
     let s = today.getSeconds();
-    let time = h + ":" + m + ":" + s;
-    if (h > 12) {h = h - 12};
-    if (h < oneStart[0] && m < oneStart[1]) {h = h - oneStart[0]; m = m - oneStart[1]; period = "Before First Period";}
-    else if (h < oneEnd[0] && m < oneEnd[1]) {h = h - oneEnd[0]; m = m - oneEnd[1]; period = "Left in First Period";}
-    else if (h < homeStart[0] && m < homeStart[1]) {h = h - homeStart[0]; m = m - homeStart[1]; period = "Before Homeroom"};
+    let totals = (h * 3600) + (m * 60) + s;
+    let times = 0
+    period = "Error no Period";
+    if (totals < oneStart) {times = oneStart - totals; period = "Before School";}
+    else if (totals < oneEnd) {times = oneEnd - totals; period = "Left of 1st Period";}
+    else if (totals < homeStart) {times = homeStart - totals; period = "Before Homeroom";}
+    else if (totals < homeEnd) {times = homeEnd - totals; period = "Left of Homeroom";}
+    else if (totals < twoStart) {times = twoStart - totals; period = "Before 2nd Period";}
+    else if (totals < twoEnd) {times = twoEnd - totals; period = "Left of 2nd Period";}
+    else if (totals < lunchStartA) {times = lunchStartA - totals; period = "Before Lunch";}
+    else if (totals < lunchEndA) {times = lunchEndA - totals; period = "Left of Lunch";}
+    else if (totals < threeStartA) {times = threeStartA - totals; period = "Before 3rd Period";}
+    else if (totals < threeEndA) {times = threeEndA - totals; period = "Left of 3rd Period";}
+    else if (totals < lunchStartB) {times = lunchStartB - totals; period = "Before Lunch";}
+    else if (totals < lunchEndB) {times = lunchEndB - totals; period = "Left of Lunch";}
+    else if (totals < threeStartB1) {times = threeStartB1 - totals; period = "Before 3rd Period";}
+    else if (totals < threeEndB1) {times = threeEndB1 - totals; period = "Left of 3rd Period";}
+    else if (totals < threeStartB2) {times = threeStartB2 - totals; period = "Before 3rd Period";}
+    else if (totals < threeEndB2) {times = threeEndB2 - totals; period = "Left of 3rd Period";}
+    else if (totals < fourStart) {times = fourStart - totals; period = "Before 4th Period";}
+    else if (totals < fourEnd) {times = fourEnd - totals; period = "Left of 4th Period";}
+    else {times = oneStart - totals; period = "After School";}
 
+    times = ((times-(times%3600))/3600) + ":" + ((times-(times%60))/60) + ":" + times%60;
+    document.getElementById('timeleft').innerHTML = times;
+    document.getElementById('period').innerHTML = period;
+    setTimeout(checkPeriod, 1000);
 
-}
+    }
